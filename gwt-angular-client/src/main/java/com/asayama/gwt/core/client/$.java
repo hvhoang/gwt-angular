2eab5267d9e04cd7d98de06218665df8c228aab3
Yuki Asayama <kyoken74@gmail.com>
2014-01-24 21:40:46 -0500

    Added interfaces to $ to support values other than String and object.

--- gwt-angular-client/src/main/java/com/asayama/gwt/core/client/$.java
+++ gwt-angular-client/src/main/java/com/asayama/gwt/core/client/$.java
@@ -1,5 +1,7 @@
 package com.asayama.gwt.core.client;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.JavaScriptObject;
 
 /**
@@ -27,6 +29,20 @@ public class $ extends JavaScriptObject {
 		return this[key]||null;
 	}-*/;
 	
+	public final native Integer getInteger(String key) /*-{
+		return this[key]||null;
+	}-*/;
+	
+	public final Long getLong(String key) {
+		String value = getString(key);
+		return value == null ? null : new Long(value);
+	}
+	
+	public final Date getDate(String key) {
+		Long value = getLong(key);
+		return value == null ? null : new Date(value);
+	}
+	
 	public final native $ set(String key, $ value) /*-{
 		this[key] = value;
 		return this;
@@ -36,4 +52,20 @@ public class $ extends JavaScriptObject {
 		this[key] = value;
 		return this;
 	}-*/;
+
+	public final native $ setInteger(String key, Integer value) /*-{
+		this[key] = value;
+		return this;
+	}-*/;
+
+	public final $ setLong(String key, Long value) {
+		setString(key, value == null ? null : value.toString());
+		return this;
+	}
+
+	public final $ setDate(String key, Date value) {
+		setLong(key, value == null ? null : value.getTime());
+		return this;
+	}
+
 }
