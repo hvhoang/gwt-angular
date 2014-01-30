b06aa371efb73ee51f28ea85292341706c35670c
Yuki Asayama <kyoken74@gmail.com>
2014-01-29 21:51:00 -0500

    Created RouteParams service. Phew... the wrapper approach worked!

---
+++ gwt-angular-client/src/main/java/com/asayama/gwt/angular/client/route/RouteParams.java
@@ -0,0 +1,64 @@
+package com.asayama.gwt.angular.client.route;
+
+import java.util.Date;
+
+import com.asayama.gwt.angular.client.Constructor;
+import com.asayama.gwt.angular.client.Service;
+import com.asayama.gwt.angular.client.Wrapper;
+import com.asayama.gwt.core.client.$;
+import com.asayama.gwt.core.client.Invoker;
+
+public class RouteParams implements Service, Wrapper<RouteParamsJSO>, Constructor {
+
+	RouteParamsJSO delegate;
+	
+	public String getString(String key) {
+		return delegate.getString(key);
+	}
+	
+	public Integer getInteger(String key) {
+		return delegate.getInteger(key);
+	}
+	
+	public Double getDouble(String key) {
+		return delegate.getDouble(key);
+	}
+	
+	public Date getDate(String key) {
+		return delegate.getDate(key);
+	}
+	
+	public <T extends $> T getObject(String key) {
+		return delegate.getObject(key);
+	}
+	
+	// Wrapper Methods
+	
+	@Override
+	public RouteParamsJSO getDelegate() {
+		return delegate;
+	}
+	
+	@Override
+	public void setDelegate($ delegate) {
+		this.delegate = delegate.cast();
+	}
+
+	// Constructor Methods
+	
+	@Override
+	public native $ constructor(Invoker invoker) /*-{
+		return [ "$routeParams",
+			function ($routeParams) {
+				invoker.@com.asayama.gwt.core.client.Invoker::invoke(Lcom/asayama/gwt/core/client/$;)($routeParams);
+				return $routeParams;
+			}];
+	}-*/;
+
+}
+class RouteParamsJSO extends $ {
+
+	protected RouteParamsJSO() {
+	}
+
+}
