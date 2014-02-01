c1a8fc7038f0ddf1f8ada621aa41ffe3c6ba9a26
Yuki Asayama <kyoken74@gmail.com>
2014-02-01 14:08:48 -0500

    Added data for POST and PUT. * Untested *

--- gwt-angular-client/src/main/java/com/asayama/gwt/angular/client/http/Http.java
+++ gwt-angular-client/src/main/java/com/asayama/gwt/angular/client/http/Http.java
@@ -14,28 +14,28 @@ public class Http extends JSObject implements Service /*, Constructor*/ {
 	}
 
 	public final <T extends JSObject> void head(String url, HttpCallback<T> callback) {
-		send("HEAD", url, callback);
+		send("HEAD", url, null, callback);
 	}
 	
 	public final <T extends JSObject> void get(String url, HttpCallback<T> callback) {
-		send("GET", url, callback);
+		send("GET", url, null, callback);
 	}
 	
-	public final <T extends JSObject> void put(String url, HttpCallback<T> callback) {
-		send("PUT", url, callback);
+	public final <T extends JSObject> void put(String url, String data, HttpCallback<T> callback) {
+		send("PUT", url, data, callback);
 	}
 	
-	public final <T extends JSObject> void post(String url, HttpCallback<T> callback) {
-		send("POST", url, callback);
+	public final <T extends JSObject> void post(String url, String data, HttpCallback<T> callback) {
+		send("POST", url, data, callback);
 	}
 	
 	public final <T extends JSObject> void delete(String url, HttpCallback<T> callback) {
-		send("DELETE", url, callback);
+		send("DELETE", url, null, callback);
 	}
 
-	final <T extends JSObject> void send(String method, final String url, final HttpCallback<T> callback) {
+	final <T extends JSObject> void send(String method, final String url, final String data, final HttpCallback<T> callback) {
 		GWT.log("[" + method + "] " + url);
-		$send(method, url, new Invoker(new Closure<HttpResponse<T>>() {
+		$send(method, url, data, new Invoker(new Closure<HttpResponse<T>>() {
 			public void closure(HttpResponse<T> response) {
 				GWT.log("[" + response.getStatus() + "] " + url);
 				try {
@@ -56,8 +56,8 @@ public class Http extends JSObject implements Service /*, Constructor*/ {
 		}));
 	}
 	
-	final native <T extends JSObject> void $send(String method, String url, Invoker successInvoker, Invoker errorInvoker) /*-{
-		this({ 'method':method, 'url':url })
+	final native <T extends JSObject> void $send(String method, String url, String data, Invoker successInvoker, Invoker errorInvoker) /*-{
+		this({ 'method':method, 'url':url, 'data':data })
 			.success(function(data, status, headers, config) {
 				successInvoker.@com.asayama.gwt.core.client.Invoker::invoke(Lcom/asayama/gwt/core/client/JSObject;)({
 					'data': data||null,
