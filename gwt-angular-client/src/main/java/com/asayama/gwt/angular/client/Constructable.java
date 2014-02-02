6d6768b1d5bb61accdc0d9b7305f77d1c402ebe9
Yuki Asayama <kyoken74@gmail.com>
2014-02-02 16:49:36 -0500

    Reinstating Constructable for wrapper objects for now.

--- gwt-angular-client/src/main/java/com/asayama/gwt/angular/client/Constructable.java
+++ gwt-angular-client/src/main/java/com/asayama/gwt/angular/client/Constructable.java
@@ -4,10 +4,7 @@ import com.asayama.gwt.core.client.Invoker;
 import com.asayama.gwt.core.client.JSObject;
 
 /**
- * This class was deprecated because it cannot be supported by JSObject 
- * extensions. When this interface was implemented by both Http and Q objects,
- * both of which are JSObect subtypes, an exception similar to the one below 
- * was thrown, and the program refused to continue.
+ * Warning: Do not implement this interface in JavaScriptObject extensions.
  * <p>
  * com.google.gwt.dev.jjs.InternalCompilerException: Already seen an 
  * implementing JSO subtype (Http) for interface (Constructor) while examining
@@ -17,7 +14,6 @@ import com.asayama.gwt.core.client.JSObject;
  * See also https://code.google.com/p/google-web-toolkit/issues/detail?id=4859
  * </p>
  */
-@Deprecated
 public interface Constructable {
 	JSObject construct(Invoker invoker);
 }
