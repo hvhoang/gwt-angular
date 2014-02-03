d5f82b48774bbddb6297e1f3b8e94a20ef7fe117
Yuki Asayama <kyoken74@gmail.com>
2014-02-02 20:03:15 -0500

    Removed GWT.log statements that were not adding much value.

--- gwt-angular-client/src/main/java/com/asayama/gwt/angular/client/q/Deferred.java
+++ gwt-angular-client/src/main/java/com/asayama/gwt/angular/client/q/Deferred.java
@@ -2,27 +2,22 @@ package com.asayama.gwt.angular.client.q;
 
 import com.asayama.gwt.angular.client.Wrapper;
 import com.asayama.gwt.core.client.JSObject;
-import com.google.gwt.core.client.GWT;
 
 public class Deferred<T extends JSObject> implements Wrapper {
 
 	public void singal(T value) {
-		GWT.log("Deferred.singal: " + (value == null ? null : value.getClass().getName()));
 		delegate.signal(value);
 	}
 	
 	public void resolve(T value) {
-		GWT.log("Deferred.resolve: " + (value == null ? null : value.getClass().getName()));
 		delegate.resolve(value);
 	}
 	
 	public void reject(T value) {
-		GWT.log("Defered.reject: " + (value == null ? null : value.getClass().getName()));
 		delegate.reject(value);
 	}
 	
 	public Promise<T> promise() {
-		GWT.log("Defered.promise");
 		return delegate.promise();
 	}
 	
