94a0671f71471a60d1dafbf4d950789c8649bd8e
Yuki Asayama <kyoken74@gmail.com>
2014-02-07 21:24:11 -0500

    JSFunction added. NOT YET TESTED

---
+++ gwt-core/src/main/java/com/asayama/gwt/core/client/JSFunction.java
@@ -0,0 +1,17 @@
+package com.asayama.gwt.core.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class JSFunction<R> extends JavaScriptObject {
+	
+	public static <R> JSFunction<R> create(Function<R> function) {
+		return _create(new Invoker(function));
+	}
+	
+	static native <R> JSFunction<R> _create(Invoker invoker) /*-{
+		return function () {
+			return invoker.@com.asayama.gwt.core.client.Invoker::invoke(Lcom/asayama/gwt/core/client/JSArray;)(arguments);
+		};
+	}-*/;
+
+}
