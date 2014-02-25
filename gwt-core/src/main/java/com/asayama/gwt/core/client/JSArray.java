d934457df0165c67e58434b2fe2ce42394c426b3
Yuki Asayama <kyoken74@gmail.com>
2014-02-24 20:39:43 -0500

    Added a lot of untested code, but cannot support the interface.
    https://github.com/kyoken74/gwt-angular/issues/28
    When I apply implements java.util.List, the app won't load.
    Nevertheless, a lot of List method implementaions (untested)
    were added.

--- gwt-core/src/main/java/com/asayama/gwt/core/client/JSArray.java
+++ gwt-core/src/main/java/com/asayama/gwt/core/client/JSArray.java
@@ -1,11 +1,15 @@
 package com.asayama.gwt.core.client;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
 
-public class JSArray<T> extends JavaScriptObject {
+public class JSArray<T> extends JavaScriptObject { //implements List<T> {
     
     public static final String[] STRING = new String[0];
 
@@ -38,13 +42,13 @@ public class JSArray<T> extends JavaScriptObject {
 		return this[i];
 	}-*/;
 	
-	public final native boolean add(T object) /*-{
-		this.push(object);
+	public final native boolean add(T e) /*-{
+		this.push(e);
 		return true;
 	}-*/;
 
-	public final native void add(int index, T object) /*-{
-		this.splice(index, 0, object);
+	public final native void add(int index, T element) /*-{
+		this.splice(index, 0, element);
 	}-*/;
 	
 	public final native int size() /*-{
@@ -66,5 +70,148 @@ public class JSArray<T> extends JavaScriptObject {
 	    }
 	    return list.toArray(a);
 	}
+
+    public final boolean addAll(Collection<? extends T> c) {
+        Iterator<? extends T> it = c.iterator();
+        boolean isChanged = false;
+        while (it.hasNext()) {
+            isChanged = isChanged || add(it.next());
+        }
+        return isChanged;
+    }
+
+    public final boolean addAll(int index, Collection<? extends T> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public final void clear() {
+        Iterator<T> it = iterator();
+        while (it.hasNext()) {
+            it.next();
+            it.remove();
+        }
+    }
+
+    public final boolean contains(Object o) {
+        Iterator<T> it = iterator();
+        while (it.hasNext()) {
+            T e = it.next();
+            if (e.equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public final boolean containsAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public final int indexOf(Object o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public final boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public final Iterator<T> iterator() {
+        return new Iterator<T>() {
+            
+            int pos = 0;
+            
+            @Override
+            public boolean hasNext() {
+                return pos < size();
+            }
+
+            @Override
+            public T next() {
+                if (pos >= size()) {
+                    throw new IndexOutOfBoundsException("index=" + pos + ", size=" + size());
+                }
+                T item = get(pos);
+                pos++;
+                return item;
+            }
+
+            @Override
+            public void remove() {
+                JSArray.this.remove(pos);
+                pos--;
+            }
+        };
+    }
+
+    public final int lastIndexOf(Object o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public final ListIterator<T> listIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public final ListIterator<T> listIterator(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public final boolean remove(Object o) {
+        Iterator<T> it = iterator();
+        while (it.hasNext()) {
+            T e = it.next();
+            if (e.equals(o)) {
+                it.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public final T remove(int index) {
+        if (index >= size()) {
+            throw new IndexOutOfBoundsException("index=" + index + ", size=" + size());
+        }
+        T result = get(index);
+        _remove(index);
+        return result;
+    }
+    
+    private final native void _remove(int index) /*-{
+        this.splice(index, 1);
+    }-*/;
+
+    public final boolean removeAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public final boolean retainAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public final T set(int index, T element) {
+        if (index >= size()) {
+            throw new IndexOutOfBoundsException("index=" + index + ", size=" + size());
+        }
+        T result = get(index);
+        _set(index, element);
+        return result;
+    }
+    
+    private final native void _set(int index, T element) /*-{
+        this[index] = element;
+    }-*/;
+
+    public final List<T> subList(int fromIndex, int toIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
