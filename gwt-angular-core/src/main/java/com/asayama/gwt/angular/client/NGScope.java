7dc614de1fcc6dd7ce916b90a472cb35aa74a68e
Yuki Asayama <kyoken74@gmail.com>
2014-02-24 19:36:01 -0500

    Reverted the Scope wrapper change around NGScope.
    https://github.com/kyoken74/gwt-angular/issues/6

--- gwt-angular-core/src/main/java/com/asayama/gwt/angular/client/NGScope.java
+++ gwt-angular-core/src/main/java/com/asayama/gwt/angular/client/NGScope.java
@@ -5,19 +5,9 @@ import com.asayama.gwt.angular.client.annotations.Bind;
 /**
  * https://github.com/kyoken74/gwt-angular/issues/6
  * @author kyoken74
- * @deprecated This class does not work.
  */
-public class Scope implements Injectable {
-    
-    private Scope ngo;
-    
-    public void digest() {
-        ngo.digest();
-    }
-}
-
 @Bind("$scope")
-class NGScope extends NGObject {
+public class NGScope extends NGObject {
 
     protected NGScope() {
     }
