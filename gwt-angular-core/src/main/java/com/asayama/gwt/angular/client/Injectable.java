474dbc5f440eb52439f9488ec2acce1d1ec955e0
Yuki Asayama <kyoken74@gmail.com>
2014-02-26 22:55:31 -0500

    Corrected an error in javadoc.

--- gwt-angular-core/src/main/java/com/asayama/gwt/angular/client/Injectable.java
+++ gwt-angular-core/src/main/java/com/asayama/gwt/angular/client/Injectable.java
@@ -2,7 +2,7 @@ package com.asayama.gwt.angular.client;
 
 /**
  * A marker interface used to flag types that are injectable into other objects,
- * such as {@link Service}s, {@link Providers}s, {@link Controllers}s, and
+ * such as {@link Service}s, {@link Provider}s, {@link Controller}s, and
  * {@link AbstractModule}s.
  * 
  * @author kyoken74
