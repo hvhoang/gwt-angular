4ce3b58880a216cbaf2eba25c65e6a96d1e21f46
Yuki Asayama <kyoken74@gmail.com>
2014-03-02 20:27:31 -0500

    0.0.19

---
+++ 0.0.19/src/main/java/com/asayama/gwt/angular/demo/client/DemoEntryPoint.java
@@ -0,0 +1,30 @@
+package com.asayama.gwt.angular.demo.client;
+
+import com.asayama.gwt.angular.client.AbstractModule;
+import com.asayama.gwt.angular.client.Angular;
+import com.asayama.gwt.angular.client.Configurator;
+import com.asayama.gwt.angular.route.client.Redirect;
+import com.asayama.gwt.angular.route.client.RouteProvider;
+import com.asayama.gwt.angular.route.client.Template;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+public class DemoEntryPoint extends AbstractModule implements EntryPoint {
+
+    public void onModuleLoad() {
+        GWT.log("initializing " + getClass().getName());
+        Angular.module(this);
+        constant("examplesURL", "examples.json");
+        controller(new NavbarController());
+        controller(new ExamplesController());
+        config(new RouteProvider(), new Configurator<RouteProvider>() {
+            @Override
+            public void configure(RouteProvider routeProvider) {
+                routeProvider
+                    .when("/examples", Template.create("partials/examples.html", ExamplesController.class))
+                    .otherwise(Redirect.create("/examples"));
+            }
+        });
+        Angular.bootstrap();
+    }
+}
