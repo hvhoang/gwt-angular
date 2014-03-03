dff96c9aa3d343b92ddbb36023195348563f2f63
Yuki Asayama <kyoken74@gmail.com>
2014-03-02 22:56:59 -0500

    0.0.20-SNAPSHOT

--- 0.0.20-SNAPSHOT/src/main/java/com/asayama/gwt/angular/demo/client/JumbotronController.java
+++ 0.0.20-SNAPSHOT/src/main/java/com/asayama/gwt/angular/demo/client/JumbotronController.java
@@ -1,57 +1,46 @@
 package com.asayama.gwt.angular.demo.client;
 
 import com.asayama.gwt.angular.client.AbstractController;
-import com.asayama.gwt.angular.client.annotations.Bind;
-import com.asayama.gwt.angular.client.location.Location;
-import com.asayama.gwt.angular.http.client.HttpClient;
-import com.asayama.gwt.angular.http.client.HttpClientCallback;
-import com.asayama.gwt.angular.route.client.RouteParams;
-import com.google.gwt.http.client.Request;
-import com.google.gwt.http.client.Response;
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.i18n.client.Constants;
 
 public class JumbotronController extends AbstractController {
 
-    // Services
-    protected HttpClient http;
-    protected RouteParams routeParams;
-    protected Location location;
+    private static JumbotronConstants CONSTANTS = GWT.create(JumbotronConstants.class);
     
-    @Bind("examplesURL")
-    private String examplesURL;
-
-    // Models
-    private Examples examples = null;
-
+    public static final String PROJECT_NAME = CONSTANTS.projectName();
+    public static final String PROJECT_DISPLAY_NAME = CONSTANTS.projectDisplayName();
+    public static final String LEARN_MORE = CONSTANTS.learnMore();
+    public static final String MORE = CONSTANTS.more();
+    public static final String EXAMPLES = CONSTANTS.examples();
+    public static final String DOWNLOAD = CONSTANTS.download();
+    public static final String DEVELOP = CONSTANTS.develop();
+    
     @Override
     public void onControllerLoad() {
-        
-        log.debug("calling ExamplesController.onControllerLoad()");
-        
-        // https://github.com/kyoken74/gwt-angular/issues/33
-        http.get(getExamplesURL(), new HttpClientCallback() {
-
-            @Override
-            public void onSuccess(Request request, Response response) {
-                examples = Examples.parse(response.getText());
-            }
-
-            @Override
-            public void onError(Request request, Exception exception) {
-            }
-        });
     }
+}
 
-    // Getters and Setters
+interface JumbotronConstants extends Constants {
+    
+    @DefaultStringValue("gwt-angular")
+    String projectName();
+    
+    @DefaultStringValue("GWT Angular")
+    String projectDisplayName();
+    
+    @DefaultStringValue("Learn more »")
+    String learnMore();
+    
+    @DefaultStringValue("More details »")
+    String more();
+    
+    @DefaultStringValue("Examples")
+    String examples();
+    
+    @DefaultStringValue("Download")
+    String download();
 
-    public Examples getExamples() {
-        return examples;
-    }
-
-    public void setExamples(Examples examples) {
-        this.examples = examples;
-    }
-
-    public String getExamplesURL() {
-        return examplesURL;
-    }
+    @DefaultStringValue("Develop")
+    String develop();
 }
