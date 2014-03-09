79b8082e140b30d12819183c1192f697eb702899
Yuki Asayama <kyoken74@gmail.com>
2014-03-09 15:08:01 -0400

    Working on the download page.

--- gwt-angular-demo/src/main/java/com/asayama/gwt/angular/demo/client/DownloadsController.java
+++ gwt-angular-demo/src/main/java/com/asayama/gwt/angular/demo/client/DownloadsController.java
@@ -1,76 +1,14 @@
 package com.asayama.gwt.angular.demo.client;
 
 import com.asayama.gwt.angular.client.Controller;
-import com.asayama.gwt.angular.client.annotations.Bind;
-import com.asayama.gwt.angular.demo.client.model.RepoTag;
-import com.asayama.gwt.angular.http.client.HttpClient;
-import com.asayama.gwt.angular.http.client.HttpClientCallback;
-import com.asayama.gwt.core.client.JSArray;
-import com.google.gwt.core.shared.GWT;
-import com.google.gwt.http.client.Request;
-import com.google.gwt.http.client.Response;
 
 public class DownloadsController implements Controller {
 
-    // Services
-    protected HttpClient http;
-    
-    @Bind("downloadsURL")
-    private String downloadsURL;
-    
-    // Models
-    private JSArray<RepoTag> previousTags;
-    private RepoTag latestTag;
-    
     @Override
     public void onControllerLoad() {
-        http.get(getDownloadsURL(), new HttpClientCallback() {
-
-            @Override
-            public void onSuccess(Request request, Response response) {
-                //TODO https://github.com/kyoken74/gwt-angular/issues/39
-                JSArray<RepoTag> tags = JSArray.eval(response.getText());
-                onTagsLoaded(tags);
-            }
-
-            @Override
-            public void onError(Request request, Exception exception) {
-                //TODO https://github.com/kyoken74/gwt-angular/issues/40
-                GWT.log(downloadsURL, exception);
-            }
-        });
     }
     
-    private void onTagsLoaded(JSArray<RepoTag> tags) {
-        if (tags == null || tags.size() == 0) {
-            GWT.log("Repository does not contain any tags");
-            return;
-        }
-        setLatestTag(tags.get(0));
-        tags.remove(0);
-        setPreviousTags(tags);
-    }
-
     // Getters and Setters
     
-    public String getDownloadsURL() {
-        return downloadsURL;
-    }
-    
-    public JSArray<RepoTag> getPreviousTags() {
-        return previousTags;
-    }
-    
-    public void setPreviousTags(JSArray<RepoTag> tags) {
-        this.previousTags = tags;
-    }
-    
-    public RepoTag getLatestTag() {
-        return latestTag;
-    }
-    
-    public void setLatestTag(RepoTag latestTag) {
-        this.latestTag = latestTag;
-    }
     
 }
