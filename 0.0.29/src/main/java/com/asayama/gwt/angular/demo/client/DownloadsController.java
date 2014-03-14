e5f01602796e811e67a9d569f8a5efcd7132bae4
Yuki Asayama <kyoken74@gmail.com>
2014-03-14 08:38:45 -0400

    0.0.29

---
+++ 0.0.29/src/main/java/com/asayama/gwt/angular/demo/client/DownloadsController.java
@@ -0,0 +1,11 @@
+package com.asayama.gwt.angular.demo.client;
+
+import com.asayama.gwt.angular.client.Controller;
+
+public class DownloadsController implements Controller {
+
+    @Override
+    public void onControllerLoad() {
+    }
+    
+}
