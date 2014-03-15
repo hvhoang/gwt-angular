e2605326d8644dfc50a39affe2ca1b08d55daa49
Yuki Asayama <kyoken74@gmail.com>
2014-03-14 22:25:44 -0400

    Commented out the list grou for now

--- gwt-angular-demo/src/main/java/com/asayama/gwt/angular/demo/client/package-info.java
+++ gwt-angular-demo/src/main/java/com/asayama/gwt/angular/demo/client/package-info.java
@@ -3,7 +3,6 @@
  */
 package com.asayama.gwt.angular.demo.client;
 
-import com.asayama.gwt.core.client.JSArray;
 import com.asayama.gwt.core.client.JSON;
 
 class Page extends JSON {
@@ -11,41 +10,12 @@ class Page extends JSON {
     protected Page() {
     }
 
-    public final JSArray<Tab> getTabs() {
-        return getJSArray("tabs");
+    public final String getTitle() {
+        return getString("title");
     }
-
-    public final void setSelectedTab(Tab tab) {
-        put("selectedTab", tab);
-    }
-
-    public final Tab getSelectedTab() {
-        return getJSObject("selectedTab");
-    }
-}
-
-class Tab extends JSON {
-
-    protected Tab() {
-    }
-
-    public final String getDisplayName() {
-        return getString("displayName");
-    }
-
-    public final String getTemplate() {
-        return getString("template");
-    }
-
+    
     public final String getFilename() {
         return getString("filename");
     }
-
-    public final String getSource() {
-        return getString("source");
-    }
-
-    public final void setSource(String source) {
-        put("source", source);
-    }
 }
+
