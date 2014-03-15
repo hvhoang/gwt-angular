75b7df5dddf09bc389b7c79b91f858a3713269b7
Yuki Asayama <kyoken74@gmail.com>
2014-03-15 12:31:17 -0400

    Starting to work on genericing the directive. Looking into ClientBundle.

--- gwt-angular-core/src/main/java/com/asayama/gwt/angular/client/Template.java
+++ gwt-angular-core/src/main/java/com/asayama/gwt/angular/client/Template.java
@@ -7,10 +7,15 @@ public class Template extends JSON {
 
     public static native Template create(String code) /*-{
         return {
+            "restrict" : "A",
             "template" : code
         };
     }-*/;
     
     protected Template() {
+    }
+    
+    public final JSON getScope() {
+        return getJSON("scope");
     }
 }
