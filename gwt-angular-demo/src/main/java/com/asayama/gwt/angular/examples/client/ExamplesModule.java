de12b46ba1d8a1f6ca99c73dd397daaad6884fa1
Yuki Asayama <kyoken74@gmail.com>
2014-03-14 22:49:53 -0400

    Further flattening the examples.
    https://github.com/kyoken74/gwt-angular/issues/44

--- gwt-angular-demo/src/main/java/com/asayama/gwt/angular/examples/client/ExamplesModule.java
+++ gwt-angular-demo/src/main/java/com/asayama/gwt/angular/examples/client/ExamplesModule.java
@@ -13,7 +13,7 @@ public class ExamplesModule extends AbstractModule implements EntryPoint {
         controller(OnClickEventExampleController.class);
         controller(PrettifyExampleController.class);
         controller(TextInputExampleController.class);
-        controller(UrlHashParameterExample.class);
+        controller(UrlHashParameterExampleController.class);
         controller(ReverseFilterExampleController.class);
     }
 }
