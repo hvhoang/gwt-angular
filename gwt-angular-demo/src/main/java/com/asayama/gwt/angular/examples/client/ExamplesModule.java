f7187649296469d328f90f0c14f125a4e871601b
Yuki Asayama <kyoken74@gmail.com>
2014-03-17 19:29:08 -0400

    Minor update for type correction.

--- gwt-angular-demo/src/main/java/com/asayama/gwt/angular/examples/client/ExamplesModule.java
+++ gwt-angular-demo/src/main/java/com/asayama/gwt/angular/examples/client/ExamplesModule.java
@@ -2,7 +2,7 @@ package com.asayama.gwt.angular.examples.client;
 
 import com.asayama.gwt.angular.client.AbstractModule;
 import com.asayama.gwt.angular.client.Angular;
-import com.asayama.gwt.core.client.JSArray;
+import com.asayama.gwt.core.client.JSON;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
@@ -12,7 +12,7 @@ public class ExamplesModule extends AbstractModule implements EntryPoint {
 
     public void onModuleLoad() {
         Angular.module(this);
-        constant("Pages.json", JSArray.eval(ExamplesResources.INSTANCE.pages().getText()));
+        constant("Pages.json", JSON.parse(ExamplesResources.INSTANCE.pages().getText()));
         filter("reverse", new ReverseFilter());
         controller(HttpClientExampleController.class);
         controller(OnClickEventExampleController.class);
