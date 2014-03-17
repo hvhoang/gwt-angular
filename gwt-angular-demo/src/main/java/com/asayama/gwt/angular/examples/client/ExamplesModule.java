65740194b76f4a8823d8230fd73f06eda9e292e9
Yuki Asayama <kyoken74@gmail.com>
2014-03-17 19:36:29 -0400

    Moved the examples to the code.
    https://github.com/kyoken74/gwt-angular/issues/44

--- gwt-angular-demo/src/main/java/com/asayama/gwt/angular/examples/client/ExamplesModule.java
+++ gwt-angular-demo/src/main/java/com/asayama/gwt/angular/examples/client/ExamplesModule.java
@@ -4,15 +4,12 @@ import com.asayama.gwt.angular.client.AbstractModule;
 import com.asayama.gwt.angular.client.Angular;
 import com.asayama.gwt.core.client.JSON;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.TextResource;
 
 public class ExamplesModule extends AbstractModule implements EntryPoint {
 
     public void onModuleLoad() {
         Angular.module(this);
-        constant("Pages.json", JSON.parse(ExamplesResources.INSTANCE.pages().getText()));
+        constant("Pages.json", getPages());
         filter("reverse", new ReverseFilter());
         controller(HttpClientExampleController.class);
         controller(OnClickEventExampleController.class);
@@ -21,12 +18,33 @@ public class ExamplesModule extends AbstractModule implements EntryPoint {
         controller(UrlHashParameterExampleController.class);
         controller(ReverseFilterExampleController.class);
     }
-}
-
-interface ExamplesResources extends ClientBundle {
     
-    public static final ExamplesResources INSTANCE = GWT.create(ExamplesResources.class);
-
-    @Source("Pages.json")
-    public TextResource pages();
+    public native JSON getPages() /*-{
+        return {
+            "textInput" : {
+                "title" : "Text Input",
+                "filename" : "partials/examples/TextInputExample.html"
+            },
+            "onClickEvent" : {
+                "title" : "OnClick Event",
+                "filename" : "partials/examples/OnClickEventExample.html"
+            },
+            "urlHashParameter" : {
+                "title" : "URL Hash Parameter",
+                "filename" : "partials/examples/UrlHashParameterExample.html"
+            },
+            "httpClient" : {
+                "title" : "HTTP Client",
+                "filename" : "partials/examples/HttpClientExample.html"
+            },
+            "reverseFilter" : {
+                "title" : "Reverse Filter",
+                "filename" : "partials/examples/ReverseFilterExample.html"
+            },
+            "prettify" : {
+                "title" : "google-code-prettify",
+                "filename" : "partials/examples/PrettifyExample.html"
+            }
+        };
+    }-*/;
 }
