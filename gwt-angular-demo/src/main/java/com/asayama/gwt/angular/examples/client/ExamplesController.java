8b2a7244f0cf3470a8a0e857450a52768bbf5c53
Yuki Asayama <kyoken74@gmail.com>
2014-03-29 10:16:15 -0400

    Abstracting the multi-page controller for Examples.
    https://github.com/kyoken74/gwt-angular/issues/50

--- gwt-angular-demo/src/main/java/com/asayama/gwt/angular/examples/client/ExamplesController.java
+++ gwt-angular-demo/src/main/java/com/asayama/gwt/angular/examples/client/ExamplesController.java
@@ -1,69 +1,36 @@
 package com.asayama.gwt.angular.examples.client;
 
-import java.util.Iterator;
-
-import com.asayama.gwt.angular.client.Controller;
 import com.asayama.gwt.angular.client.annotations.Bind;
-import com.asayama.gwt.angular.client.location.Location;
-import com.asayama.gwt.angular.route.client.RouteParams;
+import com.asayama.gwt.angular.util.client.AbstractMultiPageController;
 import com.asayama.gwt.angular.util.client.Page;
 import com.asayama.gwt.core.client.JSArray;
 
 
-public class ExamplesController implements Controller {
+public class ExamplesController extends AbstractMultiPageController {
 
-    private RouteParams routeParams;
-    private Location location;
-    private String selectedPage = null;
-    
     @Bind("examples")
     private JSArray<Page> pages = null;
-
-    @Override
-    public void onControllerLoad() {
-        selectedPage = routeParams.getString("page");
-        if (selectedPage == null || selectedPage.isEmpty() || getPage(selectedPage) == null) {
-            location.setHashParam("page", "textInput");
-        }
-    }
-    
-    public void onClickPage(Page page) {
-        location.setHashParam("page", page.getName());
-    }
+    private String defaultPageName = null;
     
     // Getters and Setters
 
-    public Page getPage(String name) {
-        Iterator<Page> it = pages.iterator();
-        while (it.hasNext()) {
-            Page p = it.next();
-            if (p.getName().equalsIgnoreCase(name)) {
-                return p;
-            }
-        }
-        return null;
+    @Override
+    public JSArray<Page> getPages() {
+        return pages;
+    }
+
+    @Override
+    public void setPages(JSArray<Page> pages) {
+        this.pages = pages;
     }
     
-    public Page getSelectedPage() {
-        return getPage(selectedPage);
+    @Override
+    public String getDefaultPageName() {
+        return defaultPageName;
     }
     
-    public Page getNextPage() {
-        Iterator<Page> it = pages.iterator();
-        boolean isNextNext = false;
-        while (it.hasNext()) {
-            Page p = it.next();
-            if (isNextNext) {
-                return p;
-            }
-            if (p.getName().equalsIgnoreCase(selectedPage)) {
-                isNextNext = true;
-            }
-        }
-        return null;
-    }
-    
-    public String getStylePageActive(String key) {
-        return key.equalsIgnoreCase(selectedPage) ? "active" : "";
+    @Override
+    public void setDefaultPageName(String defaultPageName) {
+        this.defaultPageName = defaultPageName;
     }
 }
---
+++ gwt-angular-demo/src/main/java/com/asayama/gwt/angular/util/client/AbstractMultiPageController.java
@@ -0,0 +1,94 @@
+package com.asayama.gwt.angular.util.client;
+
+import java.util.Iterator;
+
+import com.asayama.gwt.angular.client.Controller;
+import com.asayama.gwt.angular.client.location.Location;
+import com.asayama.gwt.angular.route.client.RouteParams;
+import com.asayama.gwt.core.client.JSArray;
+
+
+public abstract class AbstractMultiPageController implements Controller {
+
+    public static final String PAGE_NAME_PARAM = "page";
+    
+    private RouteParams routeParams;
+    private Location location;
+    private String selectedPageName = null;
+    
+    @Override
+    public final void onControllerLoad() {
+        setSelectedPageName(routeParams.getString(PAGE_NAME_PARAM));
+        if (isPageSelected()) {
+            getLocation().setHashParam(PAGE_NAME_PARAM, getDefaultPageName());
+        }
+    }
+    
+    public void onClickPage(Page page) {
+        getLocation().setHashParam(PAGE_NAME_PARAM, page.getName());
+    }
+    
+    public boolean isPageSelected() {
+        return (getSelectedPageName() == null
+                || getSelectedPageName().isEmpty()
+                || getPage(getSelectedPageName()) == null);
+    }
+    
+    public Page getPage(String name) {
+        Iterator<Page> it = getPages().iterator();
+        while (it.hasNext()) {
+            Page p = it.next();
+            if (p.getName().equalsIgnoreCase(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+    
+    public Page getSelectedPage() {
+        return getPage(getSelectedPageName());
+    }
+    
+    public Page getNextPage() {
+        Iterator<Page> it = getPages().iterator();
+        boolean isNextNext = false;
+        while (it.hasNext()) {
+            Page p = it.next();
+            if (isNextNext) {
+                return p;
+            }
+            if (p.getName().equalsIgnoreCase(getSelectedPageName())) {
+                isNextNext = true;
+            }
+        }
+        return null;
+    }
+    
+    public String getStylePageActive(String key) {
+        return key.equalsIgnoreCase(getSelectedPageName()) ? "active" : "";
+    }
+    
+    // Getters and Setters
+    
+    public abstract JSArray<Page> getPages();
+    public abstract void setPages(JSArray<Page> pages);
+
+    public abstract String getDefaultPageName();
+    public abstract void setDefaultPageName(String defaultPageName);
+    
+    public String getSelectedPageName() {
+        return selectedPageName;
+    }
+
+    public void setSelectedPageName(String selectedPageName) {
+        this.selectedPageName = selectedPageName;
+    }
+    
+    public Location getLocation() {
+        return location;
+    }
+    
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
