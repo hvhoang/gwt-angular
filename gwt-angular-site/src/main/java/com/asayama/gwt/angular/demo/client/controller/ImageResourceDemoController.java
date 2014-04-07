a569ab3bee139e4ed8c0de62fc98985d3320ea9f
Yuki Asayama <kyoken74@gmail.com>
2014-04-06 22:48:06 -0400

    Added image resource example text.

--- gwt-angular-site/src/main/java/com/asayama/gwt/angular/demo/client/controller/ImageResourceDemoController.java
+++ gwt-angular-site/src/main/java/com/asayama/gwt/angular/demo/client/controller/ImageResourceDemoController.java
@@ -7,10 +7,10 @@ import com.google.gwt.resources.client.ImageResource;
 
 public class ImageResourceDemoController implements Controller {
 
-    public static final ImageResource STREET = ImageSpritingDemoResources.INSTANCE.street();
-    public static final ImageResource WTC = ImageSpritingDemoResources.INSTANCE.wtc();
-    public static final ImageResource BOAT = ImageSpritingDemoResources.INSTANCE.boat();
-    public static final ImageResource PEOPLE = ImageSpritingDemoResources.INSTANCE.people();
+    public static final ImageResource STREET = ImageResourceDemoResources.INSTANCE.street();
+    public static final ImageResource WTC = ImageResourceDemoResources.INSTANCE.wtc();
+    public static final ImageResource BOAT = ImageResourceDemoResources.INSTANCE.boat();
+    public static final ImageResource PEOPLE = ImageResourceDemoResources.INSTANCE.people();
     
     @Override
     public void onControllerLoad() {
@@ -18,9 +18,9 @@ public class ImageResourceDemoController implements Controller {
     }
 }
 
-interface ImageSpritingDemoResources extends ClientBundle {
+interface ImageResourceDemoResources extends ClientBundle {
     
-    static ImageSpritingDemoResources INSTANCE = GWT.create(ImageSpritingDemoResources.class);
+    static ImageResourceDemoResources INSTANCE = GWT.create(ImageResourceDemoResources.class);
     
     @Source("com/asayama/gwt/angular/demo/client/C41-00005-17.jpg")
     ImageResource street();
