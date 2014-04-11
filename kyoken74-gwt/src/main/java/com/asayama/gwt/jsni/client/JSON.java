e9dcd5366aae2b11e160606d56b14cfd73cc1ee8
Yuki Asayama <kyoken74@gmail.com>
2014-04-11 07:37:38 -0400

    Return itself on put.

--- kyoken74-gwt/src/main/java/com/asayama/gwt/jsni/client/JSON.java
+++ kyoken74-gwt/src/main/java/com/asayama/gwt/jsni/client/JSON.java
@@ -23,100 +23,111 @@ public class JSON extends JSObject {
         return $object(key);
     }
 
-    public final <T> void put(String key, T value) {
+    public final <T> JSON put(String key, T value) {
         $object(key, value);
+        return this;
     }
     
     public final <T extends JSON> T getJSON(String key) {
         return $jsobject(key);
     }
 
-    public final void put(String key, JSON value) {
+    public final JSON put(String key, JSON value) {
         $jsobject(key, value);
+        return this;
     }
     
     public final <T extends JSObject> T getJSObject(String key) {
         return $jsobject(key);
     }
 
-    public final void put(String key, JSObject value) {
+    public final JSON put(String key, JSObject value) {
         $jsobject(key, value);
+        return this;
     }
     
     public final <T> JSArray<T> getJSArray(String key) {
         return $jsarray(key);
     }
     
-    public final <T> void put(String key, JSArray<T> value) {
+    public final <T> JSON put(String key, JSArray<T> value) {
         $jsarray(key, value);
+        return this;
     }
     
     public final <R> JSFunction<R> getJSFunction(String key) {
         return $jsobject(key) == null ? null : $jsobject(key).<JSFunction<R>>cast();
     }
 
-    public final <R> void put(String key, JSFunction<R> value) {
+    public final <R> JSON put(String key, JSFunction<R> value) {
         $object(key, value);
+        return this;
     }
     
     public final JSClosure getJSClosure(String key) {
         return $jsobject(key) == null ? null : $jsobject(key).<JSClosure>cast();
     }
 
-    public final void put(String key, JSClosure value) {
+    public final JSON put(String key, JSClosure value) {
         $object(key, value);
+        return this;
     }
     
     public final String getString(String key) {
         return $string(key);
     }
     
-    public final void put(String key, String value) {
+    public final JSON put(String key, String value) {
         $string(key, value);
+        return this;
     }
     
     public final Integer getInteger(String key) {
         return $integer(key);
     }
     
-    public final void put(String key, Integer value) {
+    public final JSON put(String key, Integer value) {
         $integer(key, value);
+        return this;
     }
 
     public final Double getDouble(String key) {
         return $double(key);
     }
 
-    public final void put(String key, Double value) {
+    public final JSON put(String key, Double value) {
         $double(key, value);
+        return this;
     }
     
     public final Boolean getBoolean(String key) {
         return $boolean(key);
     }
     
-    public final void put(String key, Boolean value) {
+    public final JSON put(String key, Boolean value) {
         $boolean(key, value);
+        return this;
     }
     
     public final Long getLong(String key) {
         return $long(key);
     }
     
-    public final void put(String key, Long value) {
+    public final JSON put(String key, Long value) {
         $long(key, value);
+        return this;
     }
     
     public final Date getDate(String key) {
         return $date(key);
     }
     
-    public final void put(String key, Date value) {
+    public final JSON put(String key, Date value) {
         $date(key, value);
+        return this;
     }
 
     public final String toJSONString() {
         return new JSONObject(this).toString();
     }
-
 }
