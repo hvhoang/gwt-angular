c0c5bc9feb525792f74494be4ef76512d736e0ce
Yuki Asayama <kyoken74@gmail.com>
2014-04-16 08:07:16 -0400

    Migrated site to hash bang.

--- gwt-angular-site/src/main/java/com/asayama/gwt/angular/site/client/SiteEntryPoint.java
+++ gwt-angular-site/src/main/java/com/asayama/gwt/angular/site/client/SiteEntryPoint.java
@@ -3,6 +3,7 @@ package com.asayama.gwt.angular.site.client;
 import com.asayama.gwt.angular.client.AbstractModule;
 import com.asayama.gwt.angular.client.Angular;
 import com.asayama.gwt.angular.client.Configurator;
+import com.asayama.gwt.angular.client.location.LocationProvider;
 import com.asayama.gwt.angular.route.client.RouteProvider;
 import com.google.gwt.core.client.EntryPoint;
 
@@ -16,10 +17,15 @@ public class SiteEntryPoint extends AbstractModule implements EntryPoint {
         controller(JumbotronController.class);
         controller(NavbarController.class);
         controller(TutorialController.class);
+        config(LocationProvider.class, new Configurator<LocationProvider>() {
+            public void configure(LocationProvider provider) {
+                provider.hashPrefix("!");
+            }
+        });
         config(RouteProvider.class, new Configurator<RouteProvider>() {
             @Override
-            public void configure(RouteProvider routeProvider) {
-                routeProvider
+            public void configure(RouteProvider provider) {
+                provider
                     .when(SiteResources.INSTANCE.documentation(), DocumentationController.class)
                     .when(SiteResources.INSTANCE.downloads(), DownloadsController.class)
                     .when(SiteResources.INSTANCE.jumbotron(), JumbotronController.class)
