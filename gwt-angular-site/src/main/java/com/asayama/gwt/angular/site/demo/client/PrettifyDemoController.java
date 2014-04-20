c2fc2d9568e219693ee024837bee3f13ef680ab1
Yuki Asayama <kyoken74@gmail.com>
2014-04-20 13:24:32 -0400

    Updated the prettify demo example demo and documentation.

--- gwt-angular-site/src/main/java/com/asayama/gwt/angular/site/demo/client/PrettifyDemoController.java
+++ gwt-angular-site/src/main/java/com/asayama/gwt/angular/site/demo/client/PrettifyDemoController.java
@@ -4,15 +4,11 @@ import com.asayama.gwt.angular.client.Controller;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.TextResource;
-import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 
 
 public class PrettifyDemoController implements Controller {
 
-    public static final String HTML_CONTENT = SafeHtmlUtils.htmlEscape(
-            PrettifyExampleResources.INSTANCE.demoHtml().getText());
-//    public static final TextResource HTML = 
-//            PrettifyExampleResources.INSTANCE.demoHtml();
+    public static final TextResource VIEW_CODE = MyResources.INSTANCE.demoHtml();
 
     @Override
     public void onControllerLoad() {
@@ -20,10 +16,9 @@ public class PrettifyDemoController implements Controller {
     }
 }
 
-interface PrettifyExampleResources extends ClientBundle {
+interface MyResources extends ClientBundle {
 
-    static final PrettifyExampleResources INSTANCE =
-            GWT.create(PrettifyExampleResources.class);
+    static final MyResources INSTANCE = GWT.create(MyResources.class);
     
     @Source("com/asayama/gwt/angular/site/demo/client/view/PrettifyDemo.html")
     TextResource demoHtml();
