c74eb8c3f371b22dc823ce8962833a83b60b5dc5
Yuki Asayama <kyoken74@gmail.com>
2014-04-20 23:26:37 -0400

    Modularized TextInputExample

--- gwt-angular-web/src/main/java/com/asayama/gwt/angular/site/examples/client/ExamplesModule.java
+++ gwt-angular-web/src/main/java/com/asayama/gwt/angular/site/examples/client/ExamplesModule.java
@@ -3,7 +3,6 @@ package com.asayama.gwt.angular.site.examples.client;
 import com.asayama.gwt.angular.client.AbstractModule;
 import com.asayama.gwt.angular.client.Angular;
 import com.asayama.gwt.angular.pages.client.Page;
-import com.asayama.gwt.angular.site.examples.client.service.GreetingService;
 import com.asayama.gwt.jsni.client.JSArray;
 import com.google.gwt.core.client.EntryPoint;
 
@@ -13,12 +12,7 @@ public class ExamplesModule extends AbstractModule implements EntryPoint {
         Angular.module(this);
         constant("examples", getPages());
         
-        factory(GreetingService.class);
-
-        controller(TextInputDemoController.class);
         controller(UrlHashParameterDemoController.class);
-
-        controller(TextInputExampleController.class);
         controller(UrlHashParameterExampleController.class);
     }
     
