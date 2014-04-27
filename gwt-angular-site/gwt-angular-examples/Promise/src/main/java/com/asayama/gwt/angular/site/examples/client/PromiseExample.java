02050f1a9022ddde5bdd9c2c02353a36d4c52222
Yuki Asayama <kyoken74@gmail.com>
2014-04-26 22:00:08 -0400

    PromiseExample module name update.

--- gwt-angular-site/gwt-angular-examples/Promise/src/main/java/com/asayama/gwt/angular/site/examples/client/PromiseExample.java
+++ gwt-angular-site/gwt-angular-examples/Promise/src/main/java/com/asayama/gwt/angular/site/examples/client/PromiseExample.java
@@ -5,7 +5,7 @@ import com.asayama.gwt.angular.client.Angular;
 import com.asayama.gwt.angular.site.examples.client.service.GreetingService;
 import com.google.gwt.core.client.EntryPoint;
 
-public class PromiseExampleModule extends AbstractModule implements EntryPoint {
+public class PromiseExample extends AbstractModule implements EntryPoint {
 
     public void onModuleLoad() {
         Angular.module(this);
