c896be9d1d80596f0ffa3c3dc5556ed996e4c549
Yuki Asayama <kyoken74@gmail.com>
2014-04-27 17:17:21 -0400

    Working on GwtHtmlResource directive. Still not working.
    The resource is loaded and inserted into the view. This much is easy.
    The problem is then applying the parent scope to this newly inserted
    HTML...

--- gwt-angular-resources/src/main/java/com/asayama/gwt/angular/resources/client/Resources.java
+++ gwt-angular-resources/src/main/java/com/asayama/gwt/angular/resources/client/Resources.java
@@ -7,22 +7,16 @@ import com.asayama.gwt.angular.resources.client.directive.GwtHtmlResource;
 import com.asayama.gwt.angular.resources.client.directive.GwtImageResource;
 import com.asayama.gwt.angular.resources.client.directive.GwtTextResource;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 
 
 public class Resources extends AbstractModule implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        String m = "initializing " + getClass().getName();
-        try {
-            Angular.module(this);
-            directive(GwtDataResource.class);
-            directive(GwtHtmlResource.class);
-            directive(GwtImageResource.class);
-            directive(GwtTextResource.class);
-        } catch (Exception e) {
-            GWT.log("Exception while " + m, e);
-        }
+        Angular.module(this);
+        directive(GwtDataResource.class);
+        directive(GwtHtmlResource.class);
+        directive(GwtImageResource.class);
+        directive(GwtTextResource.class);
     }
 }
--- gwt-angular-resources/src/main/java/com/asayama/gwt/angular/resources/client/directive/GwtHtmlResource.java
+++ gwt-angular-resources/src/main/java/com/asayama/gwt/angular/resources/client/directive/GwtHtmlResource.java
@@ -9,35 +9,39 @@ import com.asayama.gwt.jquery.client.JQElement;
 import com.asayama.gwt.jsni.client.JSON;
 import com.asayama.gwt.resources.client.HtmlResource;
 import com.google.gwt.http.client.Response;
-import com.google.gwt.safehtml.shared.SafeUri;
 
 
 public class GwtHtmlResource extends AbstractDirective {
     
     private Q q;
+    
+    /**
+     * Creates isolateScope and registers the following attribute definition.
+     * <ul>
+     * <li>{@code ImageResource} gwt-html-resource</li>
+     * </ul>
+     */
+    @Override
+    public NGScope scope() {
+    	NGScope scope = NGScope.create();
+    	scope.put(getName(), "=");
+    	return scope;
+    }
 
     @Override
     public void link(final NGScope scope, final JQElement element, JSON attrs) {
         HtmlResource resource = scope.get(getName());
         if (resource != null) {
-            SafeUri safeUri = resource.getSafeUri();
-            String url = safeUri.asString();
+            String url = resource.getSafeUri().asString();
             Promise<Response> promise = HttpUtils.get(q, url);
             promise.then(new Done<Response>() {
                 @Override
                 public void call(Response value) {
                     String text = value.getText();
                     element.append(text);
-                    element.find(">:first").addClass("ng-transclude");
                 }
             });
             return;
         }
-    }
-
-    @Override
-    public boolean getTransclude() {
-        //TODO https://github.com/kyoken74/gwt-angular/issues/62
-        return true;
     }
 }
