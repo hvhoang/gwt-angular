503a6a734e8799b3186404096bba8a06bce140ac
Yuki Asayama <kyoken74@gmail.com>
2014-04-27 18:41:00 -0400

    Working on Resource controller...

--- gwt-angular-site/gwt-angular-examples/CustomDirective/src/main/java/com/asayama/gwt/angular/site/examples/client/CustomDirectiveExampleController.java
+++ gwt-angular-site/gwt-angular-examples/CustomDirective/src/main/java/com/asayama/gwt/angular/site/examples/client/CustomDirectiveExampleController.java
@@ -7,6 +7,7 @@ import com.google.gwt.resources.client.TextResource;
 public class CustomDirectiveExampleController implements Controller {
 
     public static final String DEMO_URL = CustomDirectiveExampleResources.INSTANCE.partial().getSafeUri().asString();
+
     public static final TextResource VIEW_CODE = CustomDirectiveExampleResources.INSTANCE.view();
     public static final TextResource CONTROLLER_CODE = CustomDirectiveExampleResources.INSTANCE.controller();
     public static final TextResource DIRECTIVE_CODE = CustomDirectiveExampleResources.INSTANCE.directive();
