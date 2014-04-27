288bf01b45024674803c51fca5d0098dcbe724c4
Yuki Asayama <kyoken74@gmail.com>
2014-04-26 21:23:23 -0400

    Renamed PrettifyModule to Prettify.

--- gwt-angular-prettify/src/main/java/com/asayama/gwt/angular/prettify/client/Prettify.java
+++ gwt-angular-prettify/src/main/java/com/asayama/gwt/angular/prettify/client/Prettify.java
@@ -2,9 +2,8 @@ package com.asayama.gwt.angular.prettify.client;
 
 import com.asayama.gwt.angular.client.AbstractModule;
 import com.asayama.gwt.angular.client.Angular;
-import com.asayama.gwt.angular.prettify.client.directive.Prettyprint;
 import com.asayama.gwt.angular.prettify.client.directive.GwtPrettify;
-import com.asayama.gwt.angular.prettify.client.filter.Prettify;
+import com.asayama.gwt.angular.prettify.client.directive.Prettyprint;
 import com.asayama.gwt.jsni.client.JSObject;
 import com.asayama.gwt.resources.client.ScriptResource;
 import com.google.gwt.core.client.EntryPoint;
@@ -13,14 +12,14 @@ import com.google.gwt.dom.client.StyleInjector;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.TextResource;
 
-public class PrettifyModule extends AbstractModule implements EntryPoint {
+public class Prettify extends AbstractModule implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
         StyleInjector.inject(PrettifyScripts.INSTANCE.css().getText());
         PrettifyScripts.INSTANCE.script().ensureInjected(JSObject.$wnd);
         Angular.module(this);
-        filter(Prettify.class);
+        filter(com.asayama.gwt.angular.prettify.client.filter.Prettify.class);
         directive(GwtPrettify.class);
         directive(Prettyprint.class);
     }
