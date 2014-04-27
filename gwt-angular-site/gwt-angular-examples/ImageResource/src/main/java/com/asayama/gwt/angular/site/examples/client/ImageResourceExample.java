d4b5c751cf8664ada7e878ce464227ea0500d1eb
Yuki Asayama <kyoken74@gmail.com>
2014-04-26 21:54:09 -0400

    Renamed ImageResourceExample module name.

--- gwt-angular-site/gwt-angular-examples/ImageResource/src/main/java/com/asayama/gwt/angular/site/examples/client/ImageResourceExample.java
+++ gwt-angular-site/gwt-angular-examples/ImageResource/src/main/java/com/asayama/gwt/angular/site/examples/client/ImageResourceExample.java
@@ -4,7 +4,7 @@ import com.asayama.gwt.angular.client.AbstractModule;
 import com.asayama.gwt.angular.client.Angular;
 import com.google.gwt.core.client.EntryPoint;
 
-public class ImageResourceExampleModule extends AbstractModule implements EntryPoint {
+public class ImageResourceExample extends AbstractModule implements EntryPoint {
 
     public void onModuleLoad() {
         Angular.module(this);
