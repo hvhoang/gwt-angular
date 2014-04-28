ddef6cc20eac9ff4021a4cb542db873816fede3b
Yuki Asayama <kyoken74@gmail.com>
2014-04-28 19:50:38 -0400

    Migrating to Module.bundle()

--- gwt-angular-site/gwt-angular-examples/Promise/src/main/java/com/asayama/gwt/angular/site/examples/client/PromiseExample.java
+++ gwt-angular-site/gwt-angular-examples/Promise/src/main/java/com/asayama/gwt/angular/site/examples/client/PromiseExample.java
@@ -11,5 +11,6 @@ public class PromiseExample extends AbstractModule implements EntryPoint {
         Angular.module(this);
         factory(GreetingService.class);
         controller(PromiseController.class);
+        bundle(PromiseExampleResources.class);
     }
 }
--- gwt-angular-site/gwt-angular-examples/Promise/src/main/java/com/asayama/gwt/angular/site/examples/client/PromiseExampleController.java
+++ gwt-angular-site/gwt-angular-examples/Promise/src/main/java/com/asayama/gwt/angular/site/examples/client/PromiseExampleController.java
@@ -1,17 +1,11 @@
 package com.asayama.gwt.angular.site.examples.client;
 
 import com.asayama.gwt.angular.client.Controller;
-import com.google.gwt.resources.client.TextResource;
 
 
 public class PromiseExampleController implements Controller {
 
     public static final String DEMO_URL = PromiseExampleResources.INSTANCE.demo().getSafeUri().asString();
-    
-    public static final TextResource VIEW_CODE = PromiseExampleResources.INSTANCE.view();
-    public static final TextResource CONTROLLER_CODE = PromiseExampleResources.INSTANCE.controller();
-    public static final TextResource CONFIG_CODE = PromiseExampleResources.INSTANCE.config();
-    public static final TextResource SERVICE_CODE = PromiseExampleResources.INSTANCE.service();
     
     @Override
     public void onControllerLoad() {
