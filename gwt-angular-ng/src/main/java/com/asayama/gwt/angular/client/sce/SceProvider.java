7b6a0ddbdb3d5028fc73ef5044a3625a1d1a0078
Yuki Asayama <kyoken74@gmail.com>
2014-11-30 12:19:58 -0500

    SCE Provider to control whether SCE is enabled

--- gwt-angular-ng/src/main/java/com/asayama/gwt/angular/client/sce/SceProvider.java
+++ gwt-angular-ng/src/main/java/com/asayama/gwt/angular/client/sce/SceProvider.java
@@ -3,17 +3,12 @@ package com.asayama.gwt.angular.client.sce;
 import com.asayama.gwt.angular.client.Angular.Bind;
 import com.asayama.gwt.angular.client.NGObject;
 import com.asayama.gwt.angular.client.Provider;
-import com.google.gwt.core.client.GWT;
 
 public class SceProvider implements Provider {
 
     private NGSceProvider ngo;
 
     public boolean enabled(boolean isEnabled) {
-    	if (!isEnabled) {
-    		GWT.log("Strict Contextual Escaping (SCE) has been disabled. This "
-    				+ "may present a security risk to your application.");
-    	}
     	return ngo.enabled(isEnabled);
     }
 }
