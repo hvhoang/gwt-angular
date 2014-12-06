1795d61ee400c1f1a6b3dabaa67833cf8796ef7a
Yuki Asayama <kyoken74@gmail.com>
2014-12-06 10:01:09 -0500

    0.0.68-SNAPSHOT

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 version=0.0.68-SNAPSHOT
 
-#jar xvf ~/Developer/GitHub/gwt-angular/gwt-angular-site/gwt-angular-web/target/gwt-angular.war sitemap.xml
+#jar xvf ~/GitHub/gwt-angular/gwt-angular-site/gwt-angular-web/target/gwt-angular.war sitemap.xml
 
-jar xvf ~/Developer/GitHub/gwt-angular/gwt-angular-site/gwt-angular-web/target/gwt-angular.war site/
+jar xvf ~/GitHub/gwt-angular/gwt-angular-site/gwt-angular-web/target/gwt-angular.war site/
 
 rm -fr ${version}
 
