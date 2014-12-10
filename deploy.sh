91268ae9ac362777435634873586491b4f91da48
Yuki Asayama <kyoken74@gmail.com>
2014-12-10 08:50:20 -0500

    0.0.70-SNAPSHOT

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ version=0.0.70-SNAPSHOT
 
 jar xvf ~/GitHub/gwt-angular/gwt-angular-site/gwt-angular-web/target/gwt-angular-web-${version}.war site
 
-#node sitemap.js
-#node snapshots.js
+node sitemap.js
+node snapshots.js
 
