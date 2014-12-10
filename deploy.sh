b6fe99de9734349d4b3867143dcb658615f3e137
Yuki Asayama <kyoken74@gmail.com>
2014-12-09 19:45:45 -0500

    site is the new root

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 version=0.0.70-SNAPSHOT
 
-jar xvf ~/GitHub/gwt-angular/gwt-angular-site/gwt-angular-web/target/gwt-angular-web-${version}.war site/*
+jar xvf ~/GitHub/gwt-angular/gwt-angular-site/gwt-angular-web/target/gwt-angular-web-${version}.war site
 
-node sitemap.js
-node snapshots.js
+#node sitemap.js
+#node snapshots.js
 
