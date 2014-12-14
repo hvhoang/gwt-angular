d5cb803e67f210ab6b4007f0345a50305715a7ad
Yuki Asayama <kyoken74@gmail.com>
2014-12-14 10:27:30 -0500

    Updated controller comments

--- gwt-angular-ng/src/main/java/com/asayama/gwt/angular/client/Controller.java
+++ gwt-angular-ng/src/main/java/com/asayama/gwt/angular/client/Controller.java
@@ -11,13 +11,23 @@ public interface Controller {
 }
 
 /**
+ * Provides a bridge between GWT and AngularJS during module initialization.
+ * <p>
+ * During module initialization, controllers are registered to the module,
+ * presenting the constructor of the controller. AngularJS expects the 
+ * constructor to be provided as a JavaScript function, but during that
+ * construction time, we want to perform some initialization of the object in
+ * the GWT layer. This class enables us to perform the GWT tasks to be defined
+ * as part of that constructor.
+ * </p><p>
  * DefaultControllerContructor assumes that the first item in the dependency
  * is always "$scope", which was supplied by {@link AbstractModule}, and it
  * automatically binds the controller's properties and functions to the scope
  * according to the logic provided by {@link ControllerScopeBinderFactory}.
+ * </p>
  * 
  * @author kyoken74
- * @param <C>
+ * @param <C> Concrete implementation type of the Controller to be constructed.
  */
 class DefaultControllerConstructor<C extends Controller> extends Closure {
     
