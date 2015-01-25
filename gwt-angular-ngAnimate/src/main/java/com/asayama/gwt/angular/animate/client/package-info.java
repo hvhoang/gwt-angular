056f9cbe4955c542a64d33bad2b5a9569032b941
Yuki Asayama <kyoken74@gmail.com>
2015-01-25 14:35:59 -0500

    Cleaning up ngAnimate module

--- gwt-angular-ngAnimate/src/main/java/com/asayama/gwt/angular/animate/client/package-info.java
+++ gwt-angular-ngAnimate/src/main/java/com/asayama/gwt/angular/animate/client/package-info.java
@@ -1,12 +1,12 @@
 /**
- * Provides interfaces and implementations for AngularJS's route mechanism,
- * which is used to manage multiple views within a single-page-application.
+ * Provides interfaces and implementations for AngularJS's animation mechanism,
+ * which is used to manage various transitions of views.
  * <p>
  * In order to use the components in this module, the user must add the 
  * following inherit statements in his or code.
  * </p>
  * <pre>
- * {@code <inherits name="com.asayama.gwt.angular.route.NGRoute" />}
+ * {@code <inherits name="com.asayama.gwt.angular.animate.NGAnimate" />}
  * </pre>
  */
 package com.asayama.gwt.angular.animate.client;
