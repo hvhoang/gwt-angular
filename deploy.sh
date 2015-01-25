ff7cca63c480bef68b3a3a8466b160706d10dfe3
Yuki Asayama <kyoken74@gmail.com>
2015-01-24 21:59:32 -0500

    0.2.0

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 rm -fr site/*
 
-jar xvf ~/GitHub/gwt-angular-site/gwt-angular-web/target/gwt-angular.war site
+jar xvf ~/GitHub/gwt-angular-site/gwt-angular-web/target/gwt-angular-web-0.1.3-SNAPSHOT.war site
 
 node sitemap.js
