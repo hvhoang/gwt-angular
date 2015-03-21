17b910e227a52e7e9634b18477895906bc390ec0
Yuki Asayama <kyoken74@gmail.com>
2015-03-21 16:21:22 -0400

    Support loading code via HttpClient

--- gwt-angular-prettify/src/main/java/com/asayama/gwt/angular/prettify/client/directive/GwtPrettify.java
+++ gwt-angular-prettify/src/main/java/com/asayama/gwt/angular/prettify/client/directive/GwtPrettify.java
@@ -1,15 +1,16 @@
 package com.asayama.gwt.angular.prettify.client.directive;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.asayama.gwt.angular.client.AbstractDirective;
 import com.asayama.gwt.angular.client.Injector;
 import com.asayama.gwt.angular.client.NGScope;
+import com.asayama.gwt.angular.client.q.Promise.Done;
+import com.asayama.gwt.angular.http.client.HttpClient;
 import com.asayama.gwt.angular.prettify.client.Prettifier;
-import com.asayama.gwt.angular.prettify.client.filter.Prettify;
 import com.asayama.gwt.jquery.client.JQElement;
 import com.asayama.gwt.jsni.client.JSON;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.resources.client.TextResource;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 
@@ -39,28 +40,32 @@ import com.google.gwt.safehtml.shared.SafeHtmlUtils;
  * @author kyoken74
  */
 public class GwtPrettify extends AbstractDirective {
-
+    
     private static final String CLASS = GwtPrettify.class.getName();
     private static final Logger LOG = Logger.getLogger(CLASS);
     
     @Injector.Inject
     protected Prettifier prettifier;
     
+    @Injector.Inject
+    protected HttpClient http;
+    
     /**
      * Returns the following restrictions.
      * <ul>
-     * <li>{@link Restrict.Attribute}</li>
+     * <li>{@link Restrict.Attribute, Restrict.Element }</li>
      * </ul>
      */
     @Override
     public Restrict[] getRestrict() {
-        return new Restrict[]{ Restrict.Attribute };
+        return new Restrict[] { Restrict.Attribute, Restrict.Element };
     }
     
     /**
      * Creates isolateScope and registers the following attribute definition.
      * <ul>
-     * <li>{@code TextResource} gwt-prettify</li>
+     * <li>{@code TextResource} prettify</li>
+     * <li>{@code String} prettify</li>
      * </ul>
      */
     @Override
@@ -72,18 +77,36 @@ public class GwtPrettify extends AbstractDirective {
     
     /**
      * Replaces the element body with the TextResource passed via gwt-prettify
-     * attribute. TextResource is processed by {@link Prettify} filter.
+     * attribute. TextResource is processed by {@link Prettifier} service.
      */
     @Override
-    public void link(NGScope isolateScope, JQElement element, JSON attrs) {
-        TextResource resource = isolateScope.get(getName());
-        if (resource == null) {
-            LOG.log(Level.WARNING, "Mandatory attribute, " + getName() + ", was missing");
-            return;
+    public void link(NGScope isolateScope, final JQElement element, JSON attrs) {
+        Object o = isolateScope.get(getName());
+        if (o == null) {
+            String text = element.text();
+            append(element, text);
+        } else if (o instanceof String) {
+            String filename = (String) o;
+            http.get(filename).then(new Done<Response> () {
+
+                @Override
+                public void call(Response value) {
+                    append(element, value.getText());
+                }
+            });
+        } else if (o instanceof TextResource) {
+            TextResource resource = (TextResource) o;
+            String text = resource.getText();
+            append(element, text);
+        } else {
+            LOG.warning(getName() + " did not recognize the type of value passed to it.");
         }
-        String text = SafeHtmlUtils.htmlEscape(resource.getText());
+    }
+    
+    private void append(JQElement element, String text) {
+        String escaped = SafeHtmlUtils.htmlEscape(text);
         if (text != null && text.length() > 0) {
-            element.empty().append(prettifier.prettify(text));
+            element.empty().append(prettifier.prettify(escaped));
         }
         element.addClass("prettyprint");
     }
